d12e20364f1e8592f7c1f025f3b938afd4d064f5
akon1212233 <akonbustamante@liceovvh.cl>
2026-04-23 14:20:39 -0400

    HotFix 23/04/2026

--- python/core/funciones_internedias_(CORE)_akon_bustamante_04-03-2026.py
+++ python/core/funciones_internedias_(CORE)_akon_bustamante_04-03-2026.py
@@ -112,3 +112,21 @@ Nueva York
 Madrid
 Tokio​
 """
+
+
+
+
+datos = [
+   {"nombre": "Carlos", "puntaje": 80},
+   {"nombre": "María", "puntaje": 95},
+   {"nombre": "Pedro", "puntaje": 70}
+]
+
+datos[2]["puntaje"] = 75
+def printData(siteName : int, lista):
+    print(f"{lista[siteName]["nombre"]} obtuvo {lista[siteName]["puntaje"]} puntos")
+printData(0, datos)
+
+
+obtener_valores("nombre", datos)
+obtener_valores("puntaje", datos)
